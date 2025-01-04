bb918ed5e0a61679251dfde3279986e8e7ca0812
yusong <1079724942@qq.com>
2025-01-05 03:14:13 +0800

    Generate samples

--- Advanced/PointRend/visualize_dataset.py
+++ Advanced/PointRend/visualize_dataset.py
@@ -19,7 +19,7 @@ from detectron2.data import build_detection_test_loader
 cfg = get_cfg()
 point_rend.add_pointrend_config(cfg)
 # cfg.merge_from_file(model_zoo.get_config_file("COCO-InstanceSegmentation/mask_rcnn_R_50_FPN_3x.yaml"))
-cfg.merge_from_file("./config/pointrend_semantic_R_101_FPN_1x_cityscapes.yaml")
+cfg.merge_from_file("./configs/pointrend_semantic_R_101_FPN_1x_cityscapes.yaml")
 cfg.MODEL.RESNETS.DEPTH = 50
 cfg.DATASETS.TRAIN = ("ade20k_sem_seg_train",)
 cfg.DATASETS.TEST = ("ade20k_sem_seg_val",)
@@ -28,17 +28,17 @@ cfg.DATALOADER.NUM_WORKERS = 2
 cfg.MODEL.WEIGHTS = "detectron2://ImageNetPretrained/MSRA/R-50.pkl"
 cfg.SOLVER.IMS_PER_BATCH = 24
 cfg.SOLVER.BASE_LR = 0.0005
-cfg.SOLVER.MAX_ITER = 8000
+cfg.SOLVER.MAX_ITER = 50000
 # cfg.MODEL.ROI_HEADS.BATCH_SIZE_PER_IMAGE = 128
 # cfg.MODEL.ROI_HEADS.NUM_CLASSES = 150  # ADE20k有150个类别
 cfg.MODEL.SEM_SEG_HEAD.NUM_CLASSES = 150
 cfg.MODEL.POINT_HEAD.NUM_CLASSES = 150
 
-cfg.INPUT.MIN_SIZE_TRAIN = (512,)
-cfg.INPUT.MIN_SIZE_TEST = (512,)
+cfg.INPUT.MIN_SIZE_TRAIN = (512)
+cfg.INPUT.MIN_SIZE_TEST = (512)
 cfg.INPUT.MAX_SIZE_TRAIN = 512
 cfg.INPUT.MAX_SIZE_TEST = 512
-cfg.INPUT.CROP.ENABLED = False
+cfg.INPUT.CROP.ENABLED = True
 
 cfg.MODEL.WEIGHTS = os.path.join(cfg.OUTPUT_DIR, "model_final.pth")
 cfg.MODEL.ROI_HEADS.SCORE_THRESH_TEST = 0.5
@@ -49,12 +49,17 @@ metadata = MetadataCatalog.get("ade20k_sem_seg_val")
 
 for d in random.sample(dataset_dicts, 1):
     img = cv2.imread(d["file_name"])
+    
+    cv2.imwrite("./samples/sample_dataset_input.jpg", img)
+    
+    print("short_edge_length:", predictor.cfg.INPUT.MIN_SIZE_TEST)
+    
     outputs = predictor(img)
     outputs = outputs["sem_seg"].argmax(dim=0)
     v = Visualizer(img[:, :, ::-1], metadata=metadata, scale=0.8)
     v = v.draw_sem_seg(outputs.to("cpu"))
-    v = v.draw_dataset_dict(d)
+    # v = v.draw_dataset_dict(d)
     
-    cv2.imshow("Result", v.get_image()[:, :, ::-1])
-    cv2.waitKey(0)
-    cv2.imwrite("sample_result.jpg", v.get_image()[:, :, ::-1])
+    # cv2.imshow("Result", v.get_image()[:, :, ::-1])
+    # cv2.waitKey(0)
+    cv2.imwrite("./samples/sample_dataset_result.jpg", v.get_image()[:, :, ::-1])
