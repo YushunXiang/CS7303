a9a115d4f06137a05c0131eef2c10b26eb7db70b
yusong <1079724942@qq.com>
2025-01-05 02:30:50 +0800

    Fix config bugs, add visualization

--- Advanced/PointRend/visualize.py
+++ Advanced/PointRend/visualize.py
@@ -40,38 +40,21 @@ cfg.INPUT.MAX_SIZE_TRAIN = 512
 cfg.INPUT.MAX_SIZE_TEST = 512
 cfg.INPUT.CROP.ENABLED = False
 
-
-# 训练模型
-trainer = DefaultTrainer(cfg)
-trainer.resume_or_load(resume=False)
-trainer.train()
-
-# 验证模型
 cfg.MODEL.WEIGHTS = os.path.join(cfg.OUTPUT_DIR, "model_final.pth")
-# cfg.MODEL.WEIGHTS = model_zoo.get_checkpoint_url("COCO-InstanceSegmentation/mask_rcnn_R_50_FPN_3x.yaml")
 cfg.MODEL.ROI_HEADS.SCORE_THRESH_TEST = 0.5
 predictor = DefaultPredictor(cfg)
 
 dataset_dicts = DatasetCatalog.get("ade20k_sem_seg_val")
 metadata = MetadataCatalog.get("ade20k_sem_seg_val")
 
-# for d in random.sample(dataset_dicts, 3):
-#     img = cv2.imread(d["file_name"])
-#     outputs = predictor(img)
-#     outputs = outputs["sem_seg"].argmax(dim=0)
-#     v = Visualizer(img[:, :, ::-1], metadata=metadata, scale=0.8)
-#     v = v.draw_sem_seg(outputs.to("cpu"))
-    # v = v.draw_dataset_dict(d)
+for d in random.sample(dataset_dicts, 1):
+    img = cv2.imread(d["file_name"])
+    outputs = predictor(img)
+    outputs = outputs["sem_seg"].argmax(dim=0)
+    v = Visualizer(img[:, :, ::-1], metadata=metadata, scale=0.8)
+    v = v.draw_sem_seg(outputs.to("cpu"))
+    v = v.draw_dataset_dict(d)
     
-    # cv2.imshow("Result", v.get_image()[:, :, ::-1])
-    # cv2.waitKey(0)
-    # cv2.imwrite("result.jpg", v.get_image()[:, :, ::-1])
-
-evaluator = SemSegEvaluator("ade20k_sem_seg_val", output_dir="./output")
-val_loader = build_detection_test_loader(cfg, "ade20k_sem_seg_val")
-result = inference_on_dataset(predictor.model, val_loader, evaluator)
-
-print('mIOU: ', result['sem_seg']['mIoU'])
-print('fwIOU: ', result['sem_seg']['fwIoU'])
-print('mACC: ', result['sem_seg']['mACC'])
-print('pACC: ', result['sem_seg']['pACC'])
+    cv2.imshow("Result", v.get_image()[:, :, ::-1])
+    cv2.waitKey(0)
+    cv2.imwrite("sample_result.jpg", v.get_image()[:, :, ::-1])
